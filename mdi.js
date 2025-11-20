c1757fa282b997c1c15d125e5939bb8dc929d957
ok <gossisgreat@gmail.com>
2025-11-19 19:35:15 -0500

    Update mdi.js

--- mdi.js
+++ mdi.js
@@ -1,12 +1,5 @@
-import express from "express";
-const router = express.Router();
-
-router.post("/update", (req, res) => {
-    const data = req.body;
-
-    console.log("reponse:", data);
-
-    res.json({ status: "ok" });
-});
-
-export default router;
+{
+  "version": "roblox version", 
+  "softwareVersion": "Softwareversion", 
+  "changelogs": {}
+}
