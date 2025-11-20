8560dc1c076e26d234ab333747d1033da07b81af
ok <gossisgreat@gmail.com>
2025-11-19 19:33:43 -0500

    Modify POST /update route logging and cleanup
    Updated log message and removed commented code.

--- mdi.js
+++ mdi.js
@@ -1,13 +1,10 @@
 import express from "express";
 const router = express.Router();
 
-// POST /api/update
 router.post("/update", (req, res) => {
     const data = req.body;
 
-    console.log("Received update:", data);
-
-    // do something with the data, like send to Discord webhook
+    console.log("reponse:", data);
 
     res.json({ status: "ok" });
 });
