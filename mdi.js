38b30a87baef7a2368df9bd5285c6fc098fd925e
ok <gossisgreat@gmail.com>
2025-11-22 17:11:18 -0500

    Update software version to Beta 0.11

--- mdi.js
+++ mdi.js
@@ -1,5 +1,5 @@
 {
   "version": "version-e380c8edc8f6477c", 
-  "softwareVersion": "Beta 0.1", 
-  "changelogs": {Updated to Latested Verison}
+  "softwareVersion": "Beta 0.11", 
+  "changelogs": {}
 }
