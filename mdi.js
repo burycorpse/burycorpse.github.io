29296d1ce884a13a4d347119bf3243360e4ca3e3
ok <gossisgreat@gmail.com>
2025-12-03 16:24:27 -0500

    Update mdi.js

--- mdi.js
+++ mdi.js
@@ -1,5 +1,5 @@
 {
   "version": "version-1849ecbff0824113",
   "softwareVersion": "Beta 0.12",
-  "changelogs": ["[+] Updated to latest version"]
+  "changelogs": ["```[+] Updated to latest version```"]
 }
