810486048d2cd7b77d0e541ea2760d4598d149aa
ok <gossisgreat@gmail.com>
2025-12-04 18:26:05 -0500

    Update mdi.js

--- mdi.js
+++ mdi.js
@@ -1,5 +1,5 @@
 {
   "version": "version-1849ecbff0824113",
-  "softwareVersion": "0.13",
+  "softwareVersion": "Beta 0.13",
   "changelogs": "[+] Improved Box\n[+] Improved Assist (Camera + Ghost-Aim)\n[+] Reworked Smoothing\n[+] Fixed Ghost-Aim\n[+] Fixed Names (VISUALS)\n[+] Fixed Tools (VISUALS)\n[+] Improved Chams Outlines\n[+] Improved Response time (ASSIST, VISUALS)\n[+] Reworked Preview (VISUALS)\n[+] Added Aim Target to Orbit"
 }
