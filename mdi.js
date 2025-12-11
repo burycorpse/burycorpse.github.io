0a33566e8eb694e759bf20d6f6af242b48e4ffe8
ok <gossisgreat@gmail.com>
2025-12-11 16:29:06 -0500

    Update mdi.js

--- mdi.js
+++ mdi.js
@@ -1,5 +1,5 @@
 {
-  "version": "version-5aed1822f52c4b03",
-  "softwareVersion": "Beta 0.14",
-  "changelogs": "[+] Revamped Visuals (Name, Box, Distance, Tool, Flags)\n[+] Top/Horizontal Health Bar\n[+] Improved Cache\n[+] Decreased CPU usage significantly\n[+] Local-Player Particle Orbit\n[+] Local-Player Trail\n[+] Revamped Desync\n[+] Real-time World Edit\n[+] Target Line\n[+] Improved Default Color Schemes"
+  "version": "version-eaebe135b3ec446b",
+  "softwareVersion": "Beta 0.15",
+  "changelogs": "[+] Updated to latest verison\n[+] Tool Icon\n[+] Improvement Game Supports (Bad Business, Phantom Forces]\n[~] Repaired all known bugs"
 }
