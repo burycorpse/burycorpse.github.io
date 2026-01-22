0be261fcb84b3b0917485ef7c5866943f5ed3f6c
ok <gossisgreat@gmail.com>
2026-01-22 16:42:22 -0500

    Update mdi.js

--- mdi.js
+++ mdi.js
@@ -1,5 +1,5 @@
 {
   "version": "version-0f5223fb6e7e4955",
-  "softwareVersion": "PTesting",
-  "changelogs": "[+] version-0f5223fb6e7e4955"
+  "softwareVersion": "PTesting 2",
+  "changelogs": "[+] pslient"
 }
