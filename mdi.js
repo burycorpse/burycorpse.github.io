624e16c9d24e22745f428388903f4d2def875b09
ok <gossisgreat@gmail.com>
2026-01-27 14:47:18 -0500

    Update mdi.js

--- mdi.js
+++ mdi.js
@@ -1,8 +1,8 @@
 {
   "Platform": "Windows",
   "Versions": {
-    "Software": "PTesting 4",
+    "Software": "PTesting 5",
     "Roblox": "version-8b8c34790f814a73"
   },
-  "Changelog": "[FIX] Silent Cpu Usage\n[+] Anti-Aim Viewer\n[+] Readjustment\n[+] Triggerbot\n[+] ForceField Check\n[FIX] Ragdoll Check\n[+] Playerlist (Info, Whitelist, Tp, Spectate)\n[+] Vehicle Fly\n[+] Flip Orientation\n[+] Tickrate Modifier\n[+] Fov Changer\n[+] Rewritten Target Strafe\n[+] Look at Target\n[+] Menu Font\n[FIX] Config Saving\n[FIX] Streamproof\n[+] Name Type (Username, Display, ID)\n[FIX] Chams Drops"
+  "Changelog": "Private Changelog"
 }
