dda6ba99c4bf6278cd610a8c363d19002472b46d
ok <gossisgreat@gmail.com>
2026-01-29 20:52:32 -0500

    Update mdi.js

--- mdi.js
+++ mdi.js
@@ -1,8 +1,8 @@
 {
   "Platform": "Windows",
   "Versions": {
-    "Software": "PTesting 5",
+    "Software": "PTesting 6",
     "Roblox": "version-f8734e043e1e40a2"
   },
-  "Changelog": "Private Changelog"
+  "Changelog": "Updated."
 }
