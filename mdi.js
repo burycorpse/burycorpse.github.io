b328b05f076b2990bb02faf0e4990063abc0c2ac
ok <gossisgreat@gmail.com>
2026-02-01 16:29:10 -0500

    Update mdi.js

--- mdi.js
+++ mdi.js
@@ -1,8 +1,8 @@
 {
   "Platform": "Windows",
   "Versions": {
-    "Software": "PTesting 8",
+    "Software": "PTesting 9",
     "Roblox": "version-db4634f0e27d4d36"
   },
-  "Changelog": "[+] New Sidebar (Shift + RightClick anywhere to switch styles)"
+  "Changelog": "[+] New Sidebar (Shift + RightClick anywhere to switch styles)\n[+] Rewritten Orbit\n[+] Mesh Chams\n[+] 3d Esp Previewer\n[+] Optimizations\n[+] Improved Cache Sys"
 }
